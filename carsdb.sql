859a2f757eae3d54eb7d663bfde96068262a72d2
Mslonoff <matthew.slonoff@gmail.com>
2023-12-18 11:04:05 -0600

    updated sql

--- carsdb.sql
+++ carsdb.sql
@@ -3,17 +3,32 @@ CREATE DATABASE cardb;
 DROP TABLE IF EXISTS carData;
 
 CREATE TABLE carData (
-    car_id serial,
+    car_id serial PRIMARY KEY,
     car_make varchar(20),
     car_model varchar(30),
     car_model_year integer,
     color varchar(20),
-    mileage integer
+    mileage integer,
+    owner_id integer NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES owners (owner_id) ON DELETE CASCADE
 )
 
 CREATE TABLE owners (
-    owner_id serial,
-    car_id serial,
+    id serial PRIMARY KEY,
     first_name varchar(20),
     last_name varchar(20)
 )
+
+-- CREATE TABLE owners (
+--     id serial PRIMARY KEY,
+--     name text,
+--     age integer
+-- );
+
+-- CREATE TABLE properties (
+--     id serial PRIMARY KEY,
+--     name text,
+--     num_units integer,
+--     owner_id integer NOT NULL,
+--     FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE
+-- );
